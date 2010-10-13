9cd02a74773bf28934d9f9868cd89852b8723f47
Dereckson <dereckson@espace-win.org>
2010-10-13 18:56:05 +0000

    split() -> explode()

--- www-pamela/macs.php
+++ www-pamela/macs.php
@@ -3,6 +3,6 @@
     $macs = `/home/dereckson/scripts/user/mumble-who`;
     $macs = trim($macs);
     $macs = iconv("ISO-8859-1", "UTF-8", $macs);
-    $macs = split(" ", $macs);
+    $macs = explode(" ", $macs);
     echo json_encode($macs);
 ?>
