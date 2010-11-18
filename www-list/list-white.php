6665e32ca43c15376793e36f366e85bb28dcd8b7
Dereckson <dereckson@espace-win.org>
2010-11-18 21:08:48 +0000

    Handles correctly the web output when no player is connected.

--- www-list/list-white.php
+++ www-list/list-white.php
@@ -32,9 +32,11 @@
 function get_players () {
     //Gets players
     $players = `/home/dereckson/scripts/user/mumble-who`;
-    $players = trim($players);
-    $players = iconv("ISO-8859-1", "UTF-8", $players);
-    $players = explode(" ", $players);
+    if ($players) {
+        $players = trim($players);
+        $players = iconv("ISO-8859-1", "UTF-8", $players);
+        $players = explode(" ", $players);
+    }
 
     //Prints them
     $count = count($players);
--- www-pamela/macs.php
+++ www-pamela/macs.php
@@ -1,8 +1,12 @@
 <?php
-    //echo json_encode($macs);
-    $macs = `/home/dereckson/scripts/user/mumble-who`;
-    $macs = trim($macs);
-    $macs = iconv("ISO-8859-1", "UTF-8", $macs);
-    $macs = explode(" ", $macs);
-    echo json_encode($macs);
+    //echo json_encode($players);
+    $players = `/home/dereckson/scripts/user/mumble-who`;
+    if ($players) {
+        $players = trim($players);
+        $players = iconv("ISO-8859-1", "UTF-8", $players);
+        $players = explode(" ", $players);
+        echo json_encode($players);
+    } else {
+        echo '[]';
+    }
 ?>
